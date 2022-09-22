dd108069d1f10b16232797244f6adfc46876af4e
ZamithAhamed <zamith11@gmail.com>
2022-09-22 12:12:19 +0530

    code updated

--- MazeGame/Form1.cs
+++ MazeGame/Form1.cs
@@ -12,19 +12,34 @@ namespace MazeGame
 {
     public partial class Form1 : Form
     {
+        System.Media.SoundPlayer startSoundPlayer = new System.Media.SoundPlayer(@"C:\Users\Zamith\Music\MidwayGauntletG010.wav");
+        System.Media.SoundPlayer startSoundPlayer1 = new System.Media.SoundPlayer(@"C:\Users\Zamith\Music\swchewy.wav");
         public Form1()
         {
             InitializeComponent();
+            reset();
         }
 
         private void Label1_Click(object sender, EventArgs e)
         {
-
+            startSoundPlayer1.Play();
+            reset();
+            MessageBox.Show("Failed!!, Try Again");
         }
 
-        private void Label26_Click(object sender, EventArgs e)
+        private void FinishPoint_MouseEnter(object sender, EventArgs e)
         {
-
+            startSoundPlayer.Play();
+            MessageBox.Show("Congrats!!");
+            Close();
         }
+
+        private void reset() {
+            
+            Point startingPoint = panel1.Location;
+            startingPoint.Offset(10,10);
+            Cursor.Position = PointToScreen(startingPoint);
+        }
+
     }
 }
